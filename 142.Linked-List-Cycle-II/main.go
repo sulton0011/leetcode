cd9d264e6b3ab7a5eeb4d5918f2d0de35c419c76
Abdulaziz Kamilov <abdulaziz@MacBook-Pro-Abdulaziz.local>
2023-07-18 17:25:32 +0500

    142. Linked List Cycle II

---
+++ 142.Linked-List-Cycle-II/main.go
@@ -0,0 +1,63 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// func main() {
+// 	list1 := ListNode{
+// 		Val: 1,
+// 		Next: &ListNode{
+// 			Val: 2,
+// 			Next: &ListNode{
+// 				Val: 3,
+// 				Next: &ListNode{
+// 					Val: 4,
+// 					Next: &ListNode{
+// 						Val: 5,
+// 						Next: &ListNode{
+// 							Val: 6,
+// 						},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	}
+
+// 	resp := middleNode(&list1)
+// 	fmt.Print("Response: ")
+// 	print(resp)
+// }
+
+// func print(list *ListNode) {
+// 	if list == nil {
+// 		fmt.Println()
+// 		return
+// 	}
+// 	fmt.Print(list.Val, ", ")
+// 	print(list.Next)
+// }
+
+// code -------------------------------------------------------
+
+func middleNode(head *ListNode) *ListNode {
+
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+
+			}
+			return slow
+		}
+	}
+	return slow
+}
